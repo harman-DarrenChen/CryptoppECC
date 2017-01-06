94669b6ba78c0e9c0cd69acaaf027101b2c5debb
chendarx <darrenx.chen@intel.com>
2017-01-06 14:16:12 +0800

    init the lib target

---
+++ ExampleApp/libcryptopp/libcryptopp.h
@@ -0,0 +1,19 @@
+//
+//  libcryptopp.h
+//  libcryptopp
+//
+//  Created by DarrenX Chen on 1/6/17.
+//  Copyright © 2017 utogaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libcryptopp.
+FOUNDATION_EXPORT double libcryptoppVersionNumber;
+
+//! Project version string for libcryptopp.
+FOUNDATION_EXPORT const unsigned char libcryptoppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libcryptopp/PublicHeader.h>
+
+
